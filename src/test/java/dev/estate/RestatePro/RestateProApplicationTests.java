7477b45304b9dd6bc096a23a650aad8ed6e3053c
Ahmad Nawid <ahmadnawid.mz@gmail.com>
2025-02-25 12:02:43 +0100

    project and packages added.

---
+++ src/test/java/dev/estate/RestatePro/RestateProApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.estate.RestatePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestateProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
